fc8a90334dacc723ad61bf96b889e10aeec3f790
mamathagaddamidi <81092525+mamathagaddamidi@users.noreply.github.com>
2022-09-29 20:57:51 +0530

    Add files via upload

---
+++ src/app/interview-qa/interview-qa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interview-qa',
+  templateUrl: './interview-qa.component.html',
+  styleUrls: ['./interview-qa.component.css']
+})
+export class InterviewQAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
