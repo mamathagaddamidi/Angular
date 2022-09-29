66758209e7c1d864a1826a3796a2ef4768f08769
mamathagaddamidi <81092525+mamathagaddamidi@users.noreply.github.com>
2022-09-29 21:05:55 +0530

    Add files via upload

---
+++ src/app/py-intro/py-intro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-py-intro',
+  templateUrl: './py-intro.component.html',
+  styleUrls: ['./py-intro.component.css']
+})
+export class PyIntroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
