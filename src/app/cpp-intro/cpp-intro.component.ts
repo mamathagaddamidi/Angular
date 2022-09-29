9aea1ccbd2464351c3a2b130c9aa95cb9d910936
mamathagaddamidi <81092525+mamathagaddamidi@users.noreply.github.com>
2022-09-29 20:53:40 +0530

    Add files via upload

---
+++ src/app/cpp-intro/cpp-intro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cpp-intro',
+  templateUrl: './cpp-intro.component.html',
+  styleUrls: ['./cpp-intro.component.css']
+})
+export class CppIntroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
