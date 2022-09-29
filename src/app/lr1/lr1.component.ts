1d2686f484facf4d487b4d9879b79de26d42f43c
mamathagaddamidi <81092525+mamathagaddamidi@users.noreply.github.com>
2022-09-29 20:59:25 +0530

    Add files via upload

---
+++ src/app/lr1/lr1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lr1',
+  templateUrl: './lr1.component.html',
+  styleUrls: ['./lr1.component.css']
+})
+export class Lr1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
