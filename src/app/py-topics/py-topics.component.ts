cb9e203fd640ab0c0bc592f093b1086210f207dc
mamathagaddamidi <81092525+mamathagaddamidi@users.noreply.github.com>
2022-09-29 21:07:34 +0530

    Add files via upload

---
+++ src/app/py-topics/py-topics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-py-topics',
+  templateUrl: './py-topics.component.html',
+  styleUrls: ['./py-topics.component.css']
+})
+export class PyTopicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
