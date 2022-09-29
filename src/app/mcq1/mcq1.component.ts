a25123e051873371f45db3c08b5f9d3997f48b40
mamathagaddamidi <81092525+mamathagaddamidi@users.noreply.github.com>
2022-09-29 21:02:47 +0530

    Add files via upload

---
+++ src/app/mcq1/mcq1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mcq1',
+  templateUrl: './mcq1.component.html',
+  styleUrls: ['./mcq1.component.css']
+})
+export class Mcq1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
