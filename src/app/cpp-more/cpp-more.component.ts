5abc3bab898ce9755e71c5f44b3acbc32cb787ee
mamathagaddamidi <81092525+mamathagaddamidi@users.noreply.github.com>
2022-09-29 20:54:15 +0530

    Add files via upload

---
+++ src/app/cpp-more/cpp-more.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cpp-more',
+  templateUrl: './cpp-more.component.html',
+  styleUrls: ['./cpp-more.component.css']
+})
+export class CppMoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
