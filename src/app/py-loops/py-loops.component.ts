39e544fbb7a0e3295c90ee6dd4883f5ab84d1bd0
mamathagaddamidi <81092525+mamathagaddamidi@users.noreply.github.com>
2022-09-29 21:06:52 +0530

    Add files via upload

---
+++ src/app/py-loops/py-loops.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-py-loops',
+  templateUrl: './py-loops.component.html',
+  styleUrls: ['./py-loops.component.css']
+})
+export class PyLoopsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
