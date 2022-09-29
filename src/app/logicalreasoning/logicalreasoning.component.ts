c00976f45dab4cabf1222f73fef92834dc739ec8
mamathagaddamidi <81092525+mamathagaddamidi@users.noreply.github.com>
2022-09-29 20:58:37 +0530

    Add files via upload

---
+++ src/app/logicalreasoning/logicalreasoning.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logicalreasoning',
+  templateUrl: './logicalreasoning.component.html',
+  styleUrls: ['./logicalreasoning.component.css']
+})
+export class LogicalreasoningComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
