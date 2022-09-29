d75009ee87b49c8f500bedbdf7474ecb78fbe2fb
mamathagaddamidi <81092525+mamathagaddamidi@users.noreply.github.com>
2022-09-29 20:49:31 +0530

    Add files via upload

---
+++ src/app/c-topics/c-topics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-c-topics',
+  templateUrl: './c-topics.component.html',
+  styleUrls: ['./c-topics.component.css']
+})
+export class CTopicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
