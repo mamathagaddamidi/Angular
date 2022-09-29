164eca8276d1d2786d94ffce0b7f6c163bba0b73
mamathagaddamidi <81092525+mamathagaddamidi@users.noreply.github.com>
2022-09-29 20:48:07 +0530

    Add files via upload

---
+++ src/app/c-intro/c-intro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-c-intro',
+  templateUrl: './c-intro.component.html',
+  styleUrls: ['./c-intro.component.css']
+})
+export class CIntroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
