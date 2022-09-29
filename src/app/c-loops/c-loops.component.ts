680744e4ee3f4912b24173dbb019f76f29b1c4a0
mamathagaddamidi <81092525+mamathagaddamidi@users.noreply.github.com>
2022-09-29 20:48:50 +0530

    Add files via upload

---
+++ src/app/c-loops/c-loops.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-c-loops',
+  templateUrl: './c-loops.component.html',
+  styleUrls: ['./c-loops.component.css']
+})
+export class CLoopsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
